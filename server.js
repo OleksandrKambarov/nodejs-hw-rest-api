e6e3fffceaea73e5d9e1246d2a03a3355a103e42
OleksandrKambarov <113207904+OleksandrKambarov@users.noreply.github.com>
2023-06-30 23:03:06 +0300

    complete-server

--- server.js
+++ server.js
@@ -1,15 +1,14 @@
 const mongoose = require("mongoose");
 const app = require("./app");
 
-const DB_HOST =
-  "mongodb+srv://Oleksandr:oJFySbdLCFAExYt2@cluster0.gmmk6zx.mongodb.net/contacts_list?retryWrites=true&w=majority";
+const { DB_HOST, PORT = 3000 } = process.env;
 
 mongoose.set("strictQuery", true);
 
 mongoose
   .connect(DB_HOST)
   .then(() => {
-    app.listen(3000);
+    app.listen(PORT);
     console.log("Database connection successful");
   })
   .catch((error) => {
