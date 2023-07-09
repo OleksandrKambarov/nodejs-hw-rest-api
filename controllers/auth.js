ff42d485b5d9e33e1bfb78cb6d0b42f859cb0f7b
OleksandrKambarov <113207904+OleksandrKambarov@users.noreply.github.com>
2023-07-09 15:15:13 +0300

    change-message

--- controllers/auth.js
+++ controllers/auth.js
@@ -12,7 +12,7 @@ const register = async (req, res) => {
   const user = await User.findOne({ email });
 
   if (user) {
-    throw HttpError(409, "Email already in use");
+    throw HttpError(409, "Email in use");
   }
 
   const hashPassword = await bcrypt.hash(password, 10);
